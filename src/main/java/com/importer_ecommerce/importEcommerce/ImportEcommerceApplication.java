2cf36aa6f1425b6b8a8b27a975f3f2dc2aea6be8
alam-gir <info.alamgirhussain@gmail.com>
2025-06-14 07:06:13 +0600

    Project started.

---
+++ src/main/java/com/importer_ecommerce/importEcommerce/ImportEcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.importer_ecommerce.importEcommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImportEcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImportEcommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/importer_ecommerce/importEcommerce/ImportEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.importer_ecommerce.importEcommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImportEcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
